0a04d2a2c357f1b5eb919cad3a26e7ae2c869adc
Danny Arica Pesantes <danny.arica@avantica.net>
2016-08-02 16:21:16 -0500

    First commit: *HomeController *Octopack

---
+++ TodoWebApi/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TodoWebApi.Controllers
+{
+    [RoutePrefix("api/home")]
+    public class HomeController : ApiController
+    {
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public IHttpActionResult Get(string name)
+        {
+            return Ok("Hi " + name);
+        }
+        
+    }
+}
